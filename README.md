3bf416e8ec91e739bc8884120c0671abf56799d6
egemenyavuz <melih.egemen.yavuz@sysco.no>
2019-09-16 16:15:01 +0200

    Implemented file/blob upload

--- README.md
+++ README.md
@@ -1,4 +1,96 @@
-# azure-storage
-Sesam datasource that reads files from Azure Storage
-
 [![Build Status](https://travis-ci.org/sesam-community/azure-storage.svg?branch=master)](https://travis-ci.org/sesam-community/azure-storage)
+
+
+# azure-storage
+Can be used to
+ * reads files from Azure Storage(Blob/Files)
+ * upload files to Azure Storage(Blob/Files)
+
+ ### Environment Parameters
+
+ | CONFIG_NAME        | DESCRIPTION           | IS_REQUIRED  |DEFAULT_VALUE|
+ | -------------------|---------------------|:------------:|:-----------:|
+ | ACCOUNT_NAME | Azure Storage account name. | no, basic auth alternatively | n/a |
+ | ACCOUNT_KEY |  Azure Storage account key. | no, basic auth alternatively | n/a |
+ | SAS_PARAMS | dict for the sas_token generation parameters | no | check the code |
+
+
+ ### Query Parameters
+
+ ##### POST REQUEST:
+
+ | CONFIG_NAME        | DESCRIPTION           | IS_REQUIRED  |DEFAULT_VALUE|
+ | -------------------|---------------------|:------------:|:-----------:|
+ | start_timedelta | time offset for the start of sas_token validity. <numeric_value> followed by M, H and D for minutes, hours and days, respectively. | no | 0M (immediate start) |
+ | expiry_timedelta | time offset for the expiry of sas_token validity. <numeric_value> followed by M, H and D for minutes, hours and days, respectively.  | no | 12H |
+
+
+
+ ### An example of system config:
+
+ ```json
+ {
+   "_id": "my_azure_storage",
+   "type": "system:microservice",
+   "connect_timeout": 60,
+   "docker": {
+     "environment": {
+       "ACCOUNT_NAME":"myaccount",
+       "ACCOUNT_KEY":"myaccount-key"
+     },
+     "image": "sesamcommunity/azure-storage:latest",
+     "port": 5000
+   },
+   "read_timeout": 7200,
+ }
+ ```
+
+### An example of input-pipe config:
+```json
+  {
+    "_id": "mydataset-from-azure-storage-file",
+    "type": "pipe",
+    "source": {     
+        "type": "json",
+        "system": "my_azure_storage",
+        "url": "/file/myshare/mydir/mydataset.json"
+    },
+    "transform": {
+      "type": "dtl",
+      "rules": {
+        "default": [
+          ["copy", "*"]
+        ]
+      }
+    }
+  }
+
+  ```
+
+### An example of output-pipe config:
+Note that to uploading could be tricky due to batchsize etc. You might utilize another microservice between sesam and azure-storage for tailor-made solutions.
+ ```json
+ {
+   "_id": "mydataset-ftp-endpoint",
+   "type": "pipe",
+   "source": {
+     "type": "dataset",
+     "dataset": "mydataset-ftp"
+   },
+   "sink": {
+     "type": "json",
+     "system": "my_azure_storage",
+     "batch_size": 1000000,
+     "url": "/file/myshare/mydir/mydataset.json"
+   },
+   "transform": {
+     "type": "dtl",
+     "rules": {
+       "default": [
+         ["copy", "*"]
+       ]
+     }
+   }
+ }
+
+ ```
--- service/proxy-service.py
+++ service/proxy-service.py
@@ -1,20 +1,25 @@
 from flask import Flask, request, Response, abort, send_file
 from functools import wraps
-from azure.storage.file import FileService
-from azure.storage.blob import BlockBlobService
+from azure.storage.file import FileService, SharePermissions
+from azure.storage.blob import BlockBlobService, BlobPermissions
 import logger as logging
 import os
 import io
+import json
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 logger = logging.init_logger('azure-storage-service', os.getenv('LOGLEVEL',"INFO"))
 
+DEFAULT_SAS_PARAMS = '{"start_timedelta": null, "expiry_timedelta": "12H"}'
+
 PORT = int(os.getenv("PORT", 5000))
 ACCOUNT_NAME = os.getenv("ACCOUNT_NAME")
 ACCOUNT_KEY = os.getenv("ACCOUNT_KEY")
+SAS_PARAMS = json.loads(os.getenv("SAS_PARAMS",DEFAULT_SAS_PARAMS))
 
-logger.info('starting azure-storage-service with \n\tPORT={}\n\tACCOUNT_NAME={}\n\tLOGLEVEL={}'.format(PORT, ACCOUNT_NAME, os.getenv('LOGLEVEL',"INFO")))
+logger.info('starting azure-storage-service with \n\tPORT={}\n\tACCOUNT_NAME={}\n\tLOGLEVEL={}\n\tSAS_PARAMS={}'.format(PORT, ACCOUNT_NAME, os.getenv('LOGLEVEL',"INFO"),SAS_PARAMS))
 
 def get_auth(auth):
     if auth:
@@ -39,17 +44,110 @@ def requires_auth(f):
 
     return decorated
 
-@app.route('/file/<share_name>/<directory_name>/<file_name>', methods=['GET'])
+
+def get_sas_params(args):
+    def str_to_timedelta(str):
+        days = 0
+        hours = 0
+        minutes = 0
+        if str:
+            str = str.upper()
+            if str[-1] == 'D':
+                days = int(str[:-1])
+            elif str[-1] == 'H':
+                hours = int(str[:-1])
+            elif str[-1] == 'M':
+                minutes = int(str[:-1])
+        return timedelta(days=days, hours=hours,minutes=minutes)
+    start_timedelta_in = args.get('start_timedelta', SAS_PARAMS.get('start_timedelta'))
+    expiry_timedelta_in = args.get('expiry_timedelta', SAS_PARAMS.get('expiry_timedelta'))
+    start_timedelta_out = str_to_timedelta(start_timedelta_in)
+    expiry_timedelta_out = str_to_timedelta(expiry_timedelta_in)
+    return start_timedelta_out, expiry_timedelta_out
+
+
+def get_location(fpath):
+    path_list = fpath.split('/')
+    directory_name = "/".join(path_list[:-1]) if len(path_list) > 1 else None
+    file_name = path_list[-1]
+    return directory_name, file_name
+
+@app.route('/file/<share_name>/<path:path_to_file>', methods=['GET'])
 @requires_auth
-def get_file(share_name, directory_name, file_name):
+def get_file(share_name, path_to_file):
+        try:
+            account_name, account_key = get_auth(request.authorization)
+            directory_name, file_name = get_location(path_to_file)
+            file_service = FileService(account_name = account_name, account_key = account_key)
+            f_stream = io.BytesIO()
+            file_service.get_file_to_stream(share_name, directory_name, file_name,
+            f_stream, max_connections=6)
+            f_stream.seek(0)
+            return send_file(f_stream, attachment_filename=file_name, as_attachment=True)
+        except Exception as e:
+            logger.exception(e)
+            return abort(500, e)
+
+@app.route('/file/<share_name>/<path:path_to_file>', methods=['POST'])
+@requires_auth
+def post_file(share_name, path_to_file):
     try:
         account_name, account_key = get_auth(request.authorization)
         file_service = FileService(account_name = account_name, account_key = account_key)
-        f_stream = io.BytesIO()
-        file_service.get_file_to_stream(share_name, directory_name, file_name,
-                                               f_stream, max_connections=6)
-        f_stream.seek(0)
-        return send_file(f_stream, attachment_filename=file_name, as_attachment=True)
+        start_timedelta, expiry_timedelta = get_sas_params(request.args)
+        directory_name, file_name = get_location(path_to_file)
+        if request.headers.get('Transfer-Encoding') == 'chunked':
+            file_service.create_file_from_stream(share_name, directory_name, file_name, request.stream, count=4096)
+        else:
+            file_service.create_file_from_bytes(share_name, directory_name, file_name, request.get_data())
+        sas_token = file_service.generate_file_shared_access_signature(share_name,
+            directory_name=directory_name,
+            file_name=file_name,
+            permission=SharePermissions(read=True),
+            expiry=datetime.now() + expiry_timedelta,
+            start=start_timedelta,
+            id=None,
+            ip=None,
+            protocol='https',
+            cache_control=request.headers.get('Cache-Control'),
+            content_disposition=request.headers.get('Content-Disposition: attachment;'),
+            content_encoding=request.headers.get('Content-Encoding'),
+            content_language=request.headers.get('Content-Language'),
+            content_type=request.headers.get('Content-Type'))
+        url = file_service.make_file_url(share_name, directory_name, file_name, protocol='https', sas_token=sas_token)
+
+        return Response(response=url+"", status=200, content_type='text/plain')
+    except Exception as e:
+        logger.exception(e)
+        return abort(500, e)
+
+@app.route('/blob/<container_name>/<blob_name>', methods=['POST'])
+@requires_auth
+def post_blob(container_name, blob_name):
+    try:
+        account_name, account_key = get_auth(request.authorization)
+        file_service = BlockBlobService(account_name = account_name, account_key = account_key)
+        start_timedelta, expiry_timedelta = get_sas_params(request.args)
+        if request.headers.get('Transfer-Encoding') == 'chunked':
+            file_service.create_file_from_stream(container_name, directory_name, file_name, request.stream, count=4096)
+        else:
+            file_service.create_blob_from_bytes(container_name=container_name, blob_name=blob_name, blob=request.get_data())
+        sas_token = file_service.generate_blob_shared_access_signature(container_name=container_name,
+            blob_name=blob_name,
+            permission=BlobPermissions(read=True),
+            expiry=datetime.now() + expiry_timedelta,
+            start=start_timedelta,
+            id=None,
+            ip=None,
+            protocol='https',
+            cache_control=request.headers.get('Cache-Control'),
+            content_disposition=request.headers.get('Content-Disposition: attachment;'),
+            content_encoding=request.headers.get('Content-Encoding'),
+            content_language=request.headers.get('Content-Language'),
+            content_type=request.headers.get('Content-Type'))
+        url = file_service.make_blob_url(container_name, blob_name, protocol='https', sas_token=sas_token)
+
+        return Response(response=url+"", status=200, content_type='text/plain')
     except Exception as e:
         logger.exception(e)
         return abort(500, e)
