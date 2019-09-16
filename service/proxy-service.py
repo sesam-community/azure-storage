7bd8991d428629bf7780461848fa8c628b6f4d46
egemenyavuz <melih.egemen.yavuz@sysco.no>
2019-09-16 12:24:54 +0200

    Introduced envvars as option for authorization

--- service/proxy-service.py
+++ service/proxy-service.py
@@ -8,15 +8,19 @@ import io
 
 app = Flask(__name__)
 
+logger = logging.init_logger('azure-storage-service', os.getenv('LOGLEVEL',"INFO"))
 
-def get_var(var):
-    envvar = None
-    if var.upper() in os.environ:
-        envvar = os.environ[var.upper()]
+PORT = int(os.getenv("PORT", 5000))
+ACCOUNT_NAME = os.getenv("ACCOUNT_NAME")
+ACCOUNT_KEY = os.getenv("ACCOUNT_KEY")
+
+logger.info('starting azure-storage-service with \n\tPORT={}\n\tACCOUNT_NAME={}\n\tLOGLEVEL={}'.format(PORT, ACCOUNT_NAME, os.getenv('LOGLEVEL',"INFO")))
+
+def get_auth(auth):
+    if auth:
+        return auth.get('username'), auth.get('password')
     else:
-        envvar = request.args.get(var)
-    logger.debug("Setting %s = %s" % (var, envvar))
-    return envvar
+        return ACCOUNT_NAME, ACCOUNT_KEY
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
@@ -29,7 +33,7 @@ def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-        if not auth:
+        if not auth and not(ACCOUNT_NAME and ACCOUNT_KEY):
             return authenticate()
         return f(*args, **kwargs)
 
@@ -38,35 +42,34 @@ def requires_auth(f):
 @app.route('/file/<share_name>/<directory_name>/<file_name>', methods=['GET'])
 @requires_auth
 def get_file(share_name, directory_name, file_name):
-    auth = request.authorization
-    file_service = FileService(account_name = auth.username, account_key = auth.password)
-    f_stream = io.BytesIO()
     try:
+        account_name, account_key = get_auth(request.authorization)
+        file_service = FileService(account_name = account_name, account_key = account_key)
+        f_stream = io.BytesIO()
         file_service.get_file_to_stream(share_name, directory_name, file_name,
                                                f_stream, max_connections=6)
         f_stream.seek(0)
         return send_file(f_stream, attachment_filename=file_name, as_attachment=True)
     except Exception as e:
+        logger.exception(e)
         return abort(500, e)
 
 @app.route('/blob/<container_name>/<blob_name>', methods=['GET'])
 @requires_auth
 def get_blob(container_name, blob_name):
-    auth = request.authorization
-    blob_service = BlockBlobService(account_name = auth.username, account_key = auth.password)
-    f_stream = io.BytesIO()
     try:
+        account_name, account_key = get_auth(request.authorization)
+        blob_service = BlockBlobService(account_name = account_name, account_key = account_key)
+        f_stream = io.BytesIO()
         blob_service.get_blob_to_stream(container_name=container_name, blob_name=blob_name,
                                         stream=f_stream, max_connections=6)
         f_stream.seek(0)
         return send_file(f_stream, attachment_filename=blob_name, as_attachment=True)
     except Exception as e:
+        logger.exception(e)
         return abort(500, e)
 
 if __name__ == '__main__':
-    PORT = int(os.getenv("PORT", 5000))
-    logger = logging.init_logger('azure-storage-service', os.getenv('LOGLEVEL',"INFO"))
-
     if os.getenv('WEBFRAMEWORK', '').lower() == 'flask':
         app.run(debug=True, host='0.0.0.0', port=PORT)
     else:
